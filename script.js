086833202e5b7fbc3e7d62a5062cf7a186f8e05f
laortizt <adri26.ortiz@gmail.com>
2021-02-10 15:04:47 -0500

    ok

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-const fill = document.querySelector('')
-const empties = document.querySelectorAll('')
+const fill = document.querySelector('.fill') /*se agrega .fill*/
+const empties = document.querySelectorAll('.empty') /*se agrega .empty*/
 
-fill.addEventListener('dragstart', )
-fill.addEventListener('dragend', )
+fill.addEventListener('dragstart', dragStart) /*se agrega dragStart*/
+fill.addEventListener('dragend', dragEnd) /*se agrega dragEnd */
 
 for(const empty of empties) {
-
+    /*se agrega todo*/
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
 }
 
 function dragStart() {
-
-    setTimeout()
+    /*se agrega todo*/
+    this.className += ' hold' 
+    setTimeout(() => this.className = 'invisible', 0)
 }
 
 function dragEnd() {
-    this.
+    this.className = 'fill' /*aqui*/
 }
 
 function dragOver(e) {
-
+    e.preventDefault() /*aqui*/
 }
 
 function dragEnter(e) {
-
+    /*todo*/
+    e.preventDefault()
+    this.className += ' hovered'
 }
 
 function dragLeave() {
-
+    this.className = 'empty' /*aqui*/
 }
 
 function dragDrop() {
-
+    /*todo*/
+    this.className = 'empty'
+    this.append(fill)
 }
